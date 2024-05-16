0820aad665a31b921c1dd57ac1bd114f8baa22f6
hwang <hwangsangik@gmail.com>
2024-05-16 17:31:04 +0900

    thymelaf basic

---
+++ src/main/java/hello/thymelafbasic/ThymelafBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.thymelafbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymelafBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymelafBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/thymelafbasic/ThymelafBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.thymelafbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymelafBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
